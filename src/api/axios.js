d13d04b428b6cbcba26f3879eb0a895b7bcf249c
rtk-group <mydevclass9690@gmail.com>
2026-04-26 01:24:56 +0530

    test instately change or not

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,6 +2,8 @@ import axios from 'axios'
 
 const API_BASE_URL = import.meta.env.VITE_BACKEND_URL
 
+console.log("hello api users...")
+
 // Central Axios instance so auth + error handling stay consistent.
 const api = axios.create({
   baseURL: API_BASE_URL,
